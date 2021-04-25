67a4838d70f3f5c5a15626d135e530aaa3f5145e
Carlos Magno de França Veiga <32390057+maguinhoaju@users.noreply.github.com>
2021-04-25 10:23:45 -0300

    Add files via upload

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,49 @@
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+
+import '../../../core/app_gradients.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: "Maguinho",
+                                style: AppTextStyles.titleBold)
+                          ])),
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/32390057?v=4")),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Align(alignment: Alignment(0.0, 1.2), child: ScoreCardWidget()),
+              ],
+            ),
+          ),
+        );
+}
